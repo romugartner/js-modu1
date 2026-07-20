8437e1c10fa97d66766b9ba56fd3d6acef43a5dd
Romulo Gartner <romulogartner161114@gmail.com>
2026-07-19 21:20:48 -0300

    ejercicio 2

--- modulo_02/main.js
+++ modulo_02/main.js
@@ -1,3 +1,5 @@
+
+
 // juego de cartas: (llegar a 30 puntos)
 
 const nombre =  prompt("coloca tu nombre aqui") 
@@ -6,16 +8,19 @@ let juego = prompt("elegi el juego deseado: 1. Truco 2. Chinchon 3. Rumi")
 
 switch(juego){
     case "1":
+    alert("Jugar Truco")
     console.log("Truco");
     break;
 
 
     case "2":
+    alert("Jugar Chinchon")
     console.log("Chichon");
     break;
 
 
     case "3":
+    alert("Jugar Rumi")
     console.log("Rumi");
     break;
 
@@ -24,9 +29,4 @@ switch(juego){
         console.log("opcion invalida")
     break;
 
-}
-
-
-for(let i = 0; i <=30; i++){
-    console.log(i);
 }
